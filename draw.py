64353bce92220c3e8086d50ae6dacefe74334fe1
Shannon Lau <shannonlau00@gmail.com>
2018-04-27 07:53:34 -0400

    test

--- draw.py
+++ draw.py
@@ -336,8 +336,15 @@ def draw_line( x0, y0, z0, x1, y1, z1, screen, zbuffer, color ):
             loop_start = y1
             loop_end = y
 
+    #finding zvalues
+    if loop_end-loop_start != 0:
+        dz = (z1-z0)/(loop_end-loop_start)
+    else:
+        dz = 0
+    z = z0
+
     while ( loop_start < loop_end ):
-        plot( screen, zbuffer, color, x, y, 0 )
+        plot( screen, zbuffer, color, x, y, z )
         if ( (wide and ((A > 0 and d > 0) or (A < 0 and d < 0))) or
              (tall and ((A > 0 and d < 0) or (A < 0 and d > 0 )))):
 
@@ -349,4 +356,5 @@ def draw_line( x0, y0, z0, x1, y1, z1, screen, zbuffer, color ):
             y+= dy_east
             d+= d_east
         loop_start+= 1
-    plot( screen, zbuffer, color, x, y, 0 )
+        z += dz
+    plot( screen, zbuffer, color, x, y, z1 )
