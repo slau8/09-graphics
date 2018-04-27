b74981f75269e6ad408f7723d1ed95ddeea38b50
Shannon Lau <shannonlau00@gmail.com>
2018-04-27 01:02:34 -0400

    hm

--- draw.py
+++ draw.py
@@ -65,7 +65,6 @@ def scanline_convert(polygons, i, screen, zbuffer ):
         y += 1
 
 
-
 def add_polygon( polygons, x0, y0, z0, x1, y1, z1, x2, y2, z2 ):
     add_point(polygons, x0, y0, z0);
     add_point(polygons, x1, y1, z1);
